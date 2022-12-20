17ecb0b15759682ea61a001f7c21897b91405ec3
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 18:10:23 -0300

    Initial Commit

---
+++ MoonLight/MoonLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonLightApp.swift
+//  MoonLight
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
